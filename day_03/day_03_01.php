0d59f037ce386d44fd6f1773eb4c1fe6aab355d8
Chris Tiearney <christ@miway.co.za>
2018-12-05 22:58:31 +0200

    Added test input; Added day 3 part 1 solution;

---
+++ day_03/day_03_01.php
@@ -0,0 +1,53 @@
+<?php
+$input = explode("\n", file_get_contents('input.txt'));
+//$input = explode("\n", file_get_contents('input_test.txt'));
+
+$sheets = [];
+foreach ($input as $row) {
+    if (empty($row)) { continue; }
+
+    $parts = explode(' @ ', $row);
+
+    $id = substr($parts[0], 1);
+    $position = explode(',', substr($parts[1], 0, strpos($parts[1], ':')));
+    $size = explode('x', substr($parts[1], strpos($parts[1], ': ') + 2));
+
+    $sheets[$id] = [
+        'id' => $id,
+        'position' => ['left' => $position[0], 'top' => $position[1]],
+        'size' => ['w' => $size[0], 'h' => $size[1]],
+    ];
+}
+
+$fabric = array_fill(0, 1000, array_fill(0, 1000, '.'));
+
+foreach ($sheets as $sheet) {
+    $x = $sheet['position']['left'];
+    $y = $sheet['position']['top'];
+    $w = $sheet['size']['w'];
+    $h = $sheet['size']['h'];
+
+    for ($i = $x; $i < $x + $w; $i++) {
+        for ($j = $y; $j < $y + $h; $j++) {
+            if (empty($fabric[$j][$i])) {
+                var_dump($sheet, $x, $y, $w, $h, $id, $i, $j);
+                die;
+            }
+            if ('.' === $fabric[$j][$i]) {
+                $fabric[$j][$i] = $sheet['id'];
+            } else {
+                $fabric[$j][$i] = 'x';
+            }
+        }
+    }
+}
+
+$overlapCount = 0;
+foreach ($fabric as $row) {
+    foreach ($row as $value) {
+        if ($value === 'x') {
+            $overlapCount++;
+        }
+    }
+}
+var_dump($overlapCount);

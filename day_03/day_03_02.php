1b3b49f99ef5e1429bac749b5c6687e842b7d9cc
Chris Tiearney <christ@miway.co.za>
2018-12-05 23:06:07 +0200

    Added day 3 part 2 solution

---
+++ day_03/day_03_02.php
@@ -0,0 +1,62 @@
+<?php
+$input = explode("\n", file_get_contents('input.txt'));
+//$input = explode("\n", file_get_contents('input_test.txt'));
+
+$sheets = [];
+foreach ($input as $row) {
+    if (empty($row)) { continue; }
+
+    $parts = explode(' @ ', $row);
+
+    $id = substr($parts[0], 1);
+    $position = explode(',', substr($parts[1], 0, strpos($parts[1], ':')));
+    $size = explode('x', substr($parts[1], strpos($parts[1], ': ') + 2));
+
+    $sheets[$id] = [
+        'id' => $id,
+        'position' => ['left' => $position[0], 'top' => $position[1]],
+        'size' => ['w' => $size[0], 'h' => $size[1]],
+    ];
+}
+
+$fabric = array_fill(0, 1000, array_fill(0, 1000, '.'));
+
+foreach ($sheets as $id => $sheet) {
+    $x = $sheet['position']['left'];
+    $y = $sheet['position']['top'];
+    $w = $sheet['size']['w'];
+    $h = $sheet['size']['h'];
+
+    for ($i = $x; $i < $x + $w; $i++) {
+        for ($j = $y; $j < $y + $h; $j++) {
+            if ('.' === $fabric[$j][$i]) {
+                $fabric[$j][$i] = $sheet['id'];
+            } else {
+                $fabric[$j][$i] = 'x';
+            }
+        }
+    }
+}
+
+foreach ($sheets as $id => $sheet) {
+    $x = $sheet['position']['left'];
+    $y = $sheet['position']['top'];
+    $w = $sheet['size']['w'];
+    $h = $sheet['size']['h'];
+
+    $hasOverlap = false;
+
+    for ($i = $x; $i < $x + $w; $i++) {
+        for ($j = $y; $j < $y + $h; $j++) {
+            if ('x' === $fabric[$j][$i]) {
+                $hasOverlap = true;
+                break 2;
+            }
+        }
+    }
+
+    if (!$hasOverlap) {
+        var_dump($id);
+        break;
+    }
+}

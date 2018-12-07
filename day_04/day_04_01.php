fae8d63ddaa39c468f57d5e86da2accf004ca50a
Chris Tiearney <christ@miway.co.za>
2018-12-07 11:34:52 +0200

    Finished day 4 part 1

--- day_04/day_04_01.php
+++ day_04/day_04_01.php
@@ -2,41 +2,60 @@
 $input = file_get_contents('input.txt');
 //$input = file_get_contents('input_test.txt');
 
-$logs = [];
-$rows = array_filter(explode("\n", $input));
-foreach ($rows as $row) {
-    preg_match('/(?<=\[).*(?=\])/', $row, $matches);
-    $time = strtotime($matches[0]);
-    $logs[$time] = $row;
-}
-ksort($logs);
+$logs = array_filter(explode("\n", $input));
+sort($logs);
 
 $guards = [];
 $currentGuard = null;
 $state = null;
-foreach ($logs as $time => $row) {
-    preg_match('/(?<=\[).*(?=\])/', $row, $matches);
-    $datetime = $matches[0];
+$currentMinutes = 0;
+foreach ($logs as $row) {
+    echo "{$row}\n";
 
-    preg_match('/(?<=:).*(?=\])/', $row, $matches);
-    $minutes = $matches[0];
+    preg_match('/(?<=\[).*(?=\])/', $row, $matches);
+    $time = strtotime($matches[0]);
+    $minutes = intval(date('i', $time));
 
     preg_match('/(?<=\] ).*/', $row, $matches);
     $entry = $matches[0];
 
-    if (strpos($entry, 'Guard') !== false) {
-        preg_match('/(?<=\#)\d*/', $row, $matches);
-        $currentGuard = $matches[0];
-        $state = 'awake';
-    }
+    switch ($entry) {
+        case strpos($entry, 'Guard') !== false:
+            preg_match('/(?<=\#)\d*/', $row, $matches);
+            $currentGuard = $matches[0];
+            $state = 'awake';
 
-    if ('wakes up' === $entry) {
-        $state = 'awake';
-    }
+            if (empty($guards[$currentGuard])) {
+                $guards[$currentGuard] = array_fill(0, 60, 0);
+            }
 
-    if ('falls asleep' === $entry) {
-        $state = 'asleep';
-    }
+            break;
 
-//    $guards[$currentGuard][$minutes]
+        case 'wakes up':
+            $state = 'awake';
+            for ($i = $currentMinutes; $i < $minutes; $i++) {
+                $guards[$currentGuard][$i]++;
+            }
+            break;
+
+        case 'falls asleep':
+            $state = 'asleep';
+            $currentMinutes = $minutes;
+            break;
+    }
 }
+
+$guardSleptMost = null;
+$minutesSlept = 0;
+
+foreach ($guards as $guardId => $minutes) {
+    $totalSlept = array_sum($minutes);
+    if ($totalSlept > $minutesSlept) {
+        $guardSleptMost = $guardId;
+        $minutesSlept = $totalSlept;
+    }
+}
+
+$minuteMostSlept = array_search(max($guards[$guardSleptMost]), $guards[$guardSleptMost]);
+
+echo "#{$guardSleptMost} slept the most ({$minutesSlept} minutes slept) and mostly during minute {$minuteMostSlept}. {$guardSleptMost} * {$minuteMostSlept} = " . ($guardSleptMost * $minuteMostSlept) . "\n";

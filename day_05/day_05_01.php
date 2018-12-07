398e0681b3ddf5a4f78f41a8b2d3402b4bce0928
Chris Tiearney <christ@miway.co.za>
2018-12-07 22:38:13 +0200

    Day 5 part 1 done. Removed trailing line end from input

--- day_05/day_05_01.php
+++ day_05/day_05_01.php
@@ -1,38 +1,44 @@
 <?php
-$start = microtime();
+$start = microtime(true);
 $input = file_get_contents('input.txt');
 //$input = file_get_contents('input_test.txt');
 
-$chars = str_split($input);
+/**
+ * https://stackoverflow.com/questions/1252693/using-str-replace-so-that-it-only-acts-on-the-first-match/1252710#1252710
+ *
+ * @param $haystack
+ * @param $needle
+ * @return mixed
+ */
+function replace($haystack, $needle) {
+    $pos = strpos($haystack, $needle);
+    if ($pos !== false) {
+        return substr_replace($haystack, '', $pos, strlen($needle));
+    }
+    var_dump($haystack, $needle);
+    die('oh nohs');
+}
 
-$found = true;
-while ($found) {
+$reactions = [];
+foreach (range('a', 'z') as $char) {
+    $reactions[] = $char.strtoupper($char);
+    $reactions[] = strtoupper($char).$char;
+}
+
+$polymer = $input;
+do {
     $found = false;
-    $previousChar = '';
-//    echo implode('', $chars);
-
-    foreach ($chars as $i => $char) {
+    for ($i = 0; $i < strlen($polymer) - 1; $i++) {
+        $char = $polymer[$i];
         // credit goes to https://stackoverflow.com/a/6612519/434954 for figuring out how to flip case on a character
         $inverse = strtolower($char) ^ strtoupper($char) ^ $char;
 
-        if (
-            '' !== $previousChar &&
-            $char !== $previousChar &&
-            $previousChar === $inverse
-        ) {
-//            echo "\t{$previousChar}{$char}\n";
-            unset($chars[$i]);
-            unset($chars[$i - 1]);
+        if ($polymer[$i + 1] === $inverse && in_array($char.$inverse, $reactions)) {
+            $polymer = replace($polymer, $char.$inverse);
             $found = true;
-            break;
         }
-
-        $previousChar = $char;
     }
+} while ($found);
 
-    $chars = array_values($chars);
-}
-
-echo strlen(implode('', $chars)) . "\n";
-$end = microtime();
-echo "That took " . ($end - $start) . "\n";
+echo strlen($polymer) . PHP_EOL;
+echo "That took " . (microtime(true) - $start) . " seconds.\n";
